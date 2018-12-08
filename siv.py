e48d94961d96cc19cfd5bd3208c7b2d39ef1f18f
Pulkit Maloo <maloo.pulkit@gmail.com>
2018-12-08 22:10:40 +0000

    SIV argparser boilercode

---
+++ siv.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+
+
+def record():
+    pass
+
+def split_recording(recording):
+    """
+    recording is a wav file?
+    returns recording into 3. seconds splits
+    """
+    pass
+
+def fwd_pass(recordings, model_fname):
+    """
+    recordings is the result of split recordings
+    returns mean embedding of recordings
+    """
+    pass
+
+def store_user_embedding(username, emb):
+    """
+    this function adds username and its emb into database
+    emb is mean embedding of the recording returned from fwd_pass
+    """
+    pass
+
+def get_user_embedding(username):
+    """
+    returns user's emb from the db
+    """
+    pass
+
+def compare_user_recordings(user1_emb, other_users_emb):
+    """
+    compare a user's emb with other users emb
+    return a list of distances between user1_emb and other_users_emb
+    """
+
+def show_current_users():
+    return []
+
+def enroll_new_user():
+    print("2")
+    pass
+
+def verify_user():
+    print("3")
+    pass
+
+def identify_user():
+    print("4")
+    pass
+
+
+def main():
+    parser = ArgumentParser(description="Speaker Identification and Verification")
+    parser.add_argument('-s', '--show-current-users', dest="show",
+                        default=False, action="store_true",
+                        help="Show current enrolled users")
+    parser.add_argument('-e', '--enroll', dest="enroll",
+                        default=False, action="store_true",
+                        help="Enroll a new user")
+    parser.add_argument('-v', '--verify', dest="verify",
+                        default=False, action="store_true",
+                        help="Verify a user from the ones in the database")
+    parser.add_argument('-i', '--identify', dest="identify",
+                        default=False, action="store_true",
+                        help="Identify a user")
+    parser.add_argument('-u', '--username', type=str, default=None,
+                        help="Name of the user to enroll or verify")
+
+    args = parser.parse_args()
+
+    if args.show:
+        show_current_users()
+
+    elif args.enroll:
+        username = args.username
+        assert username is not None, "Enter username"
+        assert username not in show_current_users(), "Username already exists in database"
+        enroll_new_user(username)
+
+    elif args.verify:
+        username = args.username
+        assert username is not None, "Enter username"
+        assert username in show_current_users(), "Unrecognized username"
+        verify_user(username)
+
+
+    elif args.identify:
+        identify_user()
+
+    else:
+        show_current_users()
+
+
+if __name__ == "__main__":
+    main()
